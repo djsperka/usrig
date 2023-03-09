0bdd6b5750ffdcd30f81024ee9e431434e2e3a61
Daniel Sperka <djsperka@ucdavis.edu>
2023-03-09 11:15:58 -0800

    New demos for color vector (grating-with-distractors and tuning). Syntax fix in multigrating.

--- demo/multigrating.bat
+++ demo/multigrating.bat
@@ -1,1 +1,5 @@
-%~dp0\..\bin\remote.exe 127.0.0.1 7000 fixstim -f 0,0,1.0,red -b gray -d 813 -a -v -d 500 -f 0,0,.5,green --num-stim-pages=2 -s 0,0,3,3,100,.5,0,45,b,s,e  --multi-ori -4,4,0,4,4,45,-4,-4,90,4,-4,135/-4,4,0,4,4,45,-4,-4,90,4,-4,0/-4,4,0,4,4,45,-4,-4,90,4,-4,135/-4,4,0,4,4,45,-4,-4,0,4,-4,135/-4,4,0,4,4,45,-4,-4,90,4,-4,135/-4,4,0,4,4,0,-4,-4,90,4,-4,135/-4,4,0,4,4,45,-4,-4,90,4,-4,135/-4,4,45,4,4,0,-4,-4,0,4,-4,135
+REM Run a multi-orientation grating stimulus, using "--num-stim-pages=2", which sets up trials using 2 consecutive stimuli. 
+REM Trigger "S" gets the first stim, and trigger "U" gets the second. Advance "a" gets you the third and fourth. 
+rem You should see 4 gratings, one of which changes orientation on S/U. 
+ 
+%~dp0\..\bin\remote.exe 127.0.0.1 7000 fixstim -f 0,0,1.0,red -b gray -d 813 -a -v -d 500 -f 0,0,.5,green --num-stim-pages=2 -s 0,0,3,3,100,.5,0,45,b,s,e  --multi-ori -4,4,0,4,4,45,-4,-4,90,4,-4,135!-4,4,0,4,4,45,-4,-4,90,4,-4,0!-4,4,0,4,4,45,-4,-4,90,4,-4,135!-4,4,0,4,4,45,-4,-4,0,4,-4,135!-4,4,0,4,4,45,-4,-4,90,4,-4,135!-4,4,0,4,4,0,-4,-4,90,4,-4,135!-4,4,0,4,4,45,-4,-4,90,4,-4,135!-4,4,45,4,4,0,-4,-4,0,4,-4,135
---
+++ demo/tuning-color-vector.bat
@@ -0,0 +1,2 @@
+REM demo of box-of-parameters with a single grating. The varying parameters are Orientation (-O), color vector (--colorvector). 
+%~dp0\..\bin\remote.exe 127.0.0.1 7000 fixstim -a -f 0,0,1.0,red -b gray -d 813 -s -5,5,4,4,100,0.2,.25,0,0,b,s,e -O 0,45,90,135,180,225,270,315 --colorvector b,l,m,s,b,l,m,s 
