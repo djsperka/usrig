55ba6b12ca3a2153dbec7eebe4725f13904aade4
lab <lab@cns-dd2sh1fz1.ou.ad3.ucdavis.edu>
2024-05-29 16:05:58 -0700

    demo, conte-test-border - changed by Dan, committing so I can merge ConteTask.s2s.  -jj

--- demo/conte-test-border.bat
+++ demo/conte-test-border.bat
@@ -1,4 +1,4 @@
 REM Demo of conte stim. Uses auto-generated dot patches, and a trials file in this folder. 
 REM Run from a CommandPrompt window in the folder where this bat file lives.
 REM Use triggers F,S,X,a in fixstim server window.
-%~dp0\..\bin\remote.exe 127.0.0.1 7000 conte -v -a -b gray -d 600 -p 4 --border green,8 -f +,0,0,1,black,2,0 -c red -c green --show-cue-rects --trials %~dp0\conte-trials.csv --generate-dots 50
+%~dp0\..\bin\remote.exe 127.0.0.1 7000 conte -v -b gray -d 600 -p 4 --border green,8 -f +,0,0,1,black,2,0 -c red -c green --show-cue-rects --trials %~dp0\conte-trials.csv --generate-dots 50
